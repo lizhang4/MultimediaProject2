ff6b2d838007ee2bbc2b6bb0b1463f67598a1423
Nick <1234lizhang@gmail.com>
2021-07-19 18:26:53 +0800

    Created Content page

---
+++ content.php
@@ -0,0 +1,33 @@
+<?php
+    include_once "header.php";
+?>
+
+<section class="content-container row">
+    <div class="col-6 info-container">
+        <h1>Penang Hill</h1>
+        <div class="sub-title-container d-flex ">
+            <h2 class="d-flex flex-column">Country<span>Malaysia</span></h2>
+            <h2 class="d-flex flex-column">Region<span>Penang</span></h2>
+            <h2 class="d-flex flex-column">Since<span>1970</span></h2>
+        </div>
+        <div class="info">
+            <h2>Info</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod hic quos placeat molestiae ipsa adipisci iusto excepturi neque culpa! Odio, enim praesentium. Unde aperiam doloribus quia, dolores officia eius cumque!</p>
+        </div>
+        <div class="likes">
+            <i class="fas fa-heart"></i>
+        </div>
+    </div>
+    <div class="col-6 img ">
+        <img src="./imgs/HeroImage.jpg" alt="">
+    </div>
+
+
+</section>
+
+
+
+
+<?php
+    include_once "footer.php";
+?>
