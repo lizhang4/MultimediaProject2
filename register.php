1c1699e023f05c57120565177abab0360c125e27
Nick <1234lizhang@gmail.com>
2021-07-25 13:11:52 +0800

    Style register page

--- register.php
+++ register.php
@@ -5,41 +5,41 @@
 
 
 
-<form id="registrationForm" class="CRUD-container" enctype="multipart/form-data">
-    <h1 class=" text-center">Register</h1><hr>
+<form id="registrationForm" class="register-container d-flex flex-column justify-content-center align-items-center" enctype="multipart/form-data">
+<div class="box d-flex flex-column justify-content-center align-items-center">
+        <h1 class=" text-center my-4">Register</h1>
+        
+            <div class="mb-3">
+                <!-- <label for="username" class="form-label">Username</label> -->
+                <input class="form-control" id="username" name="usernameR" placeholder="Username">
+            </div>
+            <div class="mb-3">
+                <!-- <label for="password" class="form-label">Password</label> -->
+                <input type="text" class="form-control" id="password" name="passwordR" placeholder="Password">
+            </div>
+            <div class="mb-3">
+                <!-- <label for="repassword" class="form-label">Repeat Password</label> -->
+                <input type="text" class="form-control" id="repassword" name="repasswordR"  placeholder="Repeat Password">
+            </div>
+        
+            <div class="mb-3 gender">
+                <label for="gender" class="form-label">Gender</label>
+                <select name="genderR" id="gender" >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
 
-    <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <input class="form-control" id="username" name="usernameR">
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="text" class="form-control" id="password" name="passwordR">
-    </div>
-    <div class="mb-3">
-        <label for="repassword" class="form-label">Repeat Password</label>
-        <input type="text" class="form-control" id="repassword" name="repasswordR" >
-    </div>
+            <div class="mb-3 d-flex flex-column">
+                <!-- <label for="shortSummary" class="form-label">Short Summary</label> -->
+                <textarea name="shortSummaryR" id="shortSummary" cols="50" rows="10" placeholder="Short Summary"></textarea>
+            </div>
 
-    <div class="mb-3">
-        <label for="gender" class="form-label">Gender</label>
-        <select name="genderR" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-    </div>
+        
+            <button type="submit" class="btn btn-primary mb-3" name="register">Register</button>
+            <p class="mb-4">Already register? <a href="./login.php">Log in Here!</a></p>
 
-    <div class="mb-3 d-flex flex-column">
-        <label for="shortSummary" class="form-label">Short Summary</label>
-        <textarea name="shortSummaryR" id="shortSummary" cols="80" rows="10"></textarea>
     </div>
-
-    <!-- <div class="mb-3">
-        <label for="profileImage" class="form-label w-100">Profile Image</label>
-        <input type="file" id="profileImage" name="profileImageR" >
-    </div> -->
-    
-    <button type="submit" class="btn btn-primary" name="register">Register</button>
 </form>
 
 
